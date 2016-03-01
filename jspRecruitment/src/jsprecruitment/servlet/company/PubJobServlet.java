78e0a171e61d8b7ca34372867cee546f6e39b4c5
ycherry <1120065434@qq.com>
2016-03-01 23:17:23 +0800

    company publish job

--- jspRecruitment/src/jsprecruitment/servlet/company/PubJobServlet.java
+++ jspRecruitment/src/jsprecruitment/servlet/company/PubJobServlet.java
@@ -2,7 +2,11 @@ package jsprecruitment.servlet.company;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,44 +14,118 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import jsprecruitment.entity.Company;
 import jsprecruitment.entity.Job;
 import jsprecruitment.util.DataBaseOperation;
 
 /**
  * Servlet implementation class pubJobServlet
  */
-@WebServlet("/pubJobServlet")
+
 public class PubJobServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public PubJobServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public PubJobServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		this.doPost(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		request.setCharacterEncoding("utf-8");
-		Job job =new Job();
-		DataBaseOperation dbo=new DataBaseOperation();
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss");
-		PrintWriter out=response.getWriter();
+		Job job = new Job();
+		Date date = new Date();
+		DataBaseOperation dbo = new DataBaseOperation();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		PrintWriter out = response.getWriter();
+		// 获取网页数据
 		job.setAmount(request.getParameter("amount"));
-		//to do:获取系统时间存入数据库
+		// 获取截至时间
+		String deadline = request.getParameter("deadline");
+		java.util.Date utilDate;
+		try {
+			utilDate = sdf.parse(deadline);
+			java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+			job.setDeadline(sqlDate);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String userName=((Company)request.getSession().getAttribute("company")).getUserName();
+		job.setContents(request.getParameter("contents"));
+		job.setAddTime(new java.sql.Date(date.getTime()));
+		System.out.println("系统时间" + new java.sql.Date(date.getTime()));
+		job.setDistinct(request.getParameter("distinct"));
+		job.setEducation(request.getParameter("education"));
+		job.setExperience(request.getParameter("experience"));
+		job.setPosition(request.getParameter("position"));
+		job.setSalary(request.getParameter("salary"));
+		// 执行sql语句
+		String selectSql = "select id,companyName from t_company where userName='"
+				+ userName+ "'";
+		System.out.println("selectSql" + selectSql);
+		ResultSet rs = dbo.select(selectSql);
+		try {
+			if (rs != null && rs.getString(1) != null) {
+				String insertSql = "insert into t_company_job(cid,position,companyName,amount,distinct,education,experience,salary,contents,addtime,deadline) values ('"
+						+ rs.getInt(1)
+						+ "','"
+						+ job.getPosition()
+						+ "','"
+						+ rs.getString(2)
+						+ "','"
+						+ job.getAmount()
+						+ "','"
+						+ job.getDistinct()
+						+ "','"
+						+ job.getEducation()
+						+ "','"
+						+ job.getExperience()
+						+ "','"
+						+ job.getSalary()
+						+ "','"
+						+ job.getContents()
+						+ "','"
+						+ job.getAddTime()
+						+ "','"
+						+ job.getDeadline()
+						+ "') ";
+				System.out.println(insertSql);
+				if (dbo.insert(insertSql) > 0) {
+					System.out.println("发布职位成功！");
+					out.println("<script language='javascript' charset='utf-8' type='text/javascript'>alert('sucess');window.location.href='jobseeker/pubJob.jsp'</script>");
+				} else {
+					System.out.println("发布职位失败！");
+					out.println("<script language='javascript' charset='utf-8' type='text/javascript'>alert('fail');window.location.href='jobseeker/pubJob.jsp'</script>");
+				}
+			} else {
+				System.out.println("查询公司成功！");
+				out.println("<script language='javascript' charset='utf-8' type='text/javascript'>alert('fail');window.location.href='jobseeker/pubJob.jsp'</script>");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// to do:获取系统时间存入数据库
 	}
 
 }
